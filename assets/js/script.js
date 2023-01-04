4fe6b95ffa98137f96b85140e9c3da151bd0654a
matzhedman <hedmanmatz@gmail.com>
2023-01-04 12:27:40 +0000

    Add functions to tiles in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,118 @@
+// Function to mark tiles with X or O
+/** 
+ * Checks the tiles if they're marked with X or O
+ * After they have been marked, they will get disabled (not clickable)
+ * It is set to start the game with Player X begins and then switch to Player O
+ */
 
+playerTurn = 1;
+
+function mark_1() {
+
+	if (playerTurn == 1) {
+		document.getElementById("tile1").value = "x";
+		document.getElementById("tile1").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile1").value = "o";
+		document.getElementById("tile1").disabled = true;
+		playerTurn = 1;
+	}
+}
+
+function mark_2() {
+	if (playerTurn == 1) {
+		document.getElementById("tile2").value = "x";
+		document.getElementById("tile2").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile2").value = "o";
+		document.getElementById("tile2").disabled = true;
+		playerTurn = 1;
+	}
+}
+
+function mark_3() {
+	if (playerTurn == 1) {
+		document.getElementById("tile3").value = "x";
+		document.getElementById("tile3").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile3").value = "o";
+		document.getElementById("tile3").disabled = true;
+		playerTurn = 1;
+	}
+}
+
+function mark_4() {
+	if (playerTurn == 1) {
+		document.getElementById("tile4").value = "x";
+		document.getElementById("tile4").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile4").value = "o";
+		document.getElementById("tile4").disabled = true;
+		playerTurn = 1;
+	}
+}
+
+function mark_5() {
+	if (playerTurn == 1) {
+		document.getElementById("tile5").value = "x";
+		document.getElementById("tile5").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile5").value = "o";
+		document.getElementById("tile5").disabled = true;
+		playerTurn = 1;
+	}
+}
+
+function mark_6() {
+	if (playerTurn == 1) {
+		document.getElementById("tile6").value = "x";
+		document.getElementById("tile6").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile6").value = "o";
+		document.getElementById("tile6").disabled = true;
+		playerTurn = 1;
+	}
+}
+
+function mark_7() {
+	if (playerTurn == 1) {
+		document.getElementById("tile7").value = "x";
+		document.getElementById("tile7").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile7").value = "o";
+		document.getElementById("tile7").disabled = true;
+		playerTurn = 1;
+	}
+}
+
+function mark_8() {
+	if (playerTurn == 1) {
+		document.getElementById("tile8").value = "x";
+		document.getElementById("tile8").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile8").value = "o";
+		document.getElementById("tile8").disabled = true;
+		playerTurn = 1;
+	}
+}
+
+function mark_9() {
+	if (playerTurn == 1) {
+		document.getElementById("tile9").value = "x";
+		document.getElementById("tile9").disabled = true;
+		playerTurn = 0;
+	} else {
+		document.getElementById("tile9").value = "o";
+		document.getElementById("tile9").disabled = true;
+		playerTurn = 1;
+	}
+}
 
